d06a15a242c46d01d5648f584a104a2b41fb4b7a
NanoXyin Official <vanr7gaming@gmail.com>
2026-07-31 15:31:56 +0700

    Delete src/lib/utils.ts (via batch delete)

--- src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-export function cn(...inputs: ClassValue[]) { return twMerge(clsx(inputs)) }
-export function generateId(): string { return Math.random().toString(36).substring(2,15)+Math.random().toString(36).substring(2,15) }
-export function delay(ms: number): Promise<void> { return new Promise(r=>setTimeout(r,ms)) }
-export function formatTime(date: string|Date): string { return new Date(date).toLocaleTimeString('id-ID',{hour:'2-digit',minute:'2-digit'}) }
